c44c70e8c2dbefc008f08c5a75765bfde7a68917
Paul Parton <paul@paulparton.com>
2013-11-12 00:33:58 +1100

    resource update

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -1,32 +1,25 @@
 'use strict';
 
-angular.module('confAppApp').controller('AddCtrl', function ($scope, $http, $routeParams, $location) {
+angular.module('confAppApp').controller('AddCtrl', function ($scope, $http, $routeParams, $location, ConfSessions) {
 
     $scope.newSession = {};
 
     $scope.saveSession = function(){
 
-       var newSessionData = $scope.newSession;
-
-        $http({method:'post', url:'/api/collection/', data: newSessionData}).
-                success(function(data, status, headers, config){
+        ConfSessions.save(newSessionData, function(data){
 
                     
                     console.log(data);
 
                     //Add the collection of items to the $scope
                     $scope.items = data;
+                    
+        });
 
-                }).
-                error(function(data, status, headers, config){
 
-                    $scope.items = "";
+       var newSessionData = $scope.newSession;
 
-                    console.log('Error communicating with server: ' + status);
-                    console.log(headers);
-                    console.log(data);
 
-                });
 
     };
 
